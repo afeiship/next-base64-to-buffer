8f4e846c99b292a9cf08dad2181cd4e16c83b0fc
afeiship <1290657123@qq.com>
2019-07-09 21:11:00 +0800

    fix: b64

--- dist/next-base64-to-buffer.js
+++ dist/next-base64-to-buffer.js
@@ -2,19 +2,17 @@
  * name: next-base64-to-buffer
  * url: https://github.com/afeiship/next-base64-to-buffer
  * version: 1.0.0
- * date: 2019-07-09T13:10:07.814Z
+ * date: 2019-07-09T13:10:55.905Z
  * license: MIT
  */
 
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
-  var FUNC = 'function';
+  var B64 = 'base64';
 
   nx.base64ToBuffer = function(inDataURL) {
-    var buf =
-      typeof Buffer.from === FUNC ? Buffer.from(inDataURL, 'base64') : new Buffer(inDataURL);
-    return buf.toString('base64');
+    return Buffer.from(inDataURL, B64);
   };
 
   if (typeof module !== 'undefined' && module.exports) {
--- src/next-base64-to-buffer.js
+++ src/next-base64-to-buffer.js
@@ -1,12 +1,10 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
-  var FUNC = 'function';
+  var B64 = 'base64';
 
   nx.base64ToBuffer = function(inDataURL) {
-    var buf =
-      typeof Buffer.from === FUNC ? Buffer.from(inDataURL, 'base64') : new Buffer(inDataURL);
-    return buf.toString('base64');
+    return Buffer.from(inDataURL, B64);
   };
 
   if (typeof module !== 'undefined' && module.exports) {
